65653663b0bb3a96d7b3b7299739bf9c65e10f38
nekodigi <uekaz@outlook.jp>
2023-09-13 11:00:08 +0900

    url fix

---
+++ docs/_next/static/chunks/app/test/page-46463f0ae91080b5.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[928],{3708:function(e,r,t){Promise.resolve().then(t.bind(t,8237))},8237:function(e,r,t){"use strict";t.r(r),t.d(r,{default:function(){return u}});var n=t(7437),s=t(2265),o=t(8526);function u(){let[e,r]=(0,s.useState)(""),{ref:t}=(0,o.T_)({onDecodeResult(e){r(e.getText())}});return(0,n.jsxs)(n.Fragment,{children:[(0,n.jsx)("video",{ref:t}),(0,n.jsxs)("p",{children:[(0,n.jsx)("span",{children:"Last result:"}),(0,n.jsx)("span",{children:e})]})]})}},622:function(e,r,t){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=t(2265),s=Symbol.for("react.element"),o=Symbol.for("react.fragment"),u=Object.prototype.hasOwnProperty,f=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,c={key:!0,ref:!0,__self:!0,__source:!0};function i(e,r,t){var n,o={},i=null,l=null;for(n in void 0!==t&&(i=""+t),void 0!==r.key&&(i=""+r.key),void 0!==r.ref&&(l=r.ref),r)u.call(r,n)&&!c.hasOwnProperty(n)&&(o[n]=r[n]);if(e&&e.defaultProps)for(n in r=e.defaultProps)void 0===o[n]&&(o[n]=r[n]);return{$$typeof:s,type:e,key:i,ref:l,props:o,_owner:f.current}}r.Fragment=o,r.jsx=i,r.jsxs=i},7437:function(e,r,t){"use strict";e.exports=t(622)}},function(e){e.O(0,[526,971,596,744],function(){return e(e.s=3708)}),_N_E=e.O()}]);
